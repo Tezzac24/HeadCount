65859fb0878f209b29612de6bcf2d49715672486
Tezzac24 <83923814+Tezzac24@users.noreply.github.com>
2024-05-02 14:45:43 +0100

    Update db_manager.py

--- Api/db_manager.py
+++ Api/db_manager.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+
 api = Api(app) 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
